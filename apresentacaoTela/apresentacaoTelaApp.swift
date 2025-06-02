b8e539aac9100922d27bcd0bb3c9ec5265d09a89
arefdeveloper91 <149487876+arefdeveloper91@users.noreply.github.com>
2025-06-02 19:37:44 -0300

    Initial Commit

---
+++ apresentacaoTela/apresentacaoTelaApp.swift
@@ -0,0 +1,17 @@
+//
+//  apresentacaoTelaApp.swift
+//  apresentacaoTela
+//
+//  Created by Aref Chucri on 02/06/25.
+//
+
+import SwiftUI
+
+@main
+struct apresentacaoTelaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
